fe2c384248b249024d11ed8f1e988767b4001703
Kacper <kacper.piaskowski@wp.pl>
2023-05-06 19:30:12 +0200

    added login service updated login view

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -7,10 +7,16 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   private _registerUrl = "http://localhost:3000/register";
+  private _loginUrl = "http://localhost:3000/login";
 
   constructor(private http: HttpClient) { }
 
   registerUser(user: any) {
     return this.http.post<any>(this._registerUrl, user)
   }
+
+  loginUser(user: any) {
+    return this.http.post<any>(this._loginUrl, user)
+  }
+
 }
--- frontend/src/app/login-view/login-view.component.ts
+++ frontend/src/app/login-view/login-view.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.css']
 })
-export class LoginViewComponent {
+export class LoginViewComponent implements OnInit {
+
+  loginUserData = {email: '', password: ''}
+  constructor(private _auth: AuthService){}
+
+  loginUser(){
+    this._auth.loginUser(this.loginUserData)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
+  }
+
+
+  ngOnInit(){
+
+  }
+
 
 }
