b032bf145c8baabddd929f8f461485460ffe4e2a
piaskowskikacper <94793177+piaskowskikacper@users.noreply.github.com>
2023-06-15 17:25:29 +0200

    Update mongoose.js
    hidden mongodb uri before setting repo to public

--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://m001-student:Ip5IFT3atKrlQ3tu@sandbox.7vudx.mongodb.net/upscores?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('xxxmongodbdburihere', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to database"))
     .catch((error) => console.log(error));
 
